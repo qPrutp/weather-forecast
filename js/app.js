96354a9998cc39accaaa05292b4ff89a7405335c
Vasyl Savitskyy <vasylsavitskyy@gmail.com>
2018-07-29 23:43:31 +0300

    added first request

--- js/app.js
+++ js/app.js
@@ -4,62 +4,103 @@ const cities = ["Lviv", "Kyiv", "London", "New York"];
 const temperatures = ["C", "F"];
 const apiKey = "5140914bfb0847d1752cd225b5311a51";
 
-function Form(name, content) {
+function Form(arrCity, arrTemp) {
 	let newForm;
 	let newSelect;
 	let newOption;
 	let valueOption;
+	let inputRange;
+	let dataList;
 
 	newSelect = document.createElement("select");
-	if (name === "city") {
-		newSelect.setAttribute("onclick", "loadData()");
-	}
+	newSelect.setAttribute("onmouseup", "loadData(this.value)");
 
-	for(let index in content) {
+	for(let index in arrCity) {
 		newOption = document.createElement("option");
 		if (index == 0 ) {
 			newOption.setAttribute("selected", "selected");
 		}
-		newOption.setAttribute("value", content[index]);
-		valueOption = document.createTextNode(content[index]);
+		newOption.setAttribute("value", arrCity[index]);
+		valueOption = document.createTextNode(arrCity[index]);
 		newOption.appendChild(valueOption);
 		newSelect.appendChild(newOption);
 	}
 
-	newForm = document.createElement("form");
+	inputRange = document.createElement("input");
+	inputRange.setAttribute("id", "formSlider");
+	inputRange.setAttribute("type", "range");
+	inputRange.setAttribute("list", "tempList");
+	dataList = document.createElement("datalist");
+	dataList.setAttribute("id", "tempList");
+	for(let index in arrTemp) {
+		newOption = document.createElement("option");
+		if (index == 0) {
+			newOption.setAttribute("selected", "selected");
+		}
+		newOption.setAttribute("value", index);
+		dataList.appendChild(newOption);
+	}
 
+	inputRange.setAttribute("value", arrTemp.indexOf("C"));
+	inputRange.setAttribute("step", "1");
+	inputRange.setAttribute("min", arrTemp.indexOf("C"));
+	inputRange.setAttribute("max", arrTemp.length - 1);
+
+	newForm = document.createElement("form");
 	newForm.appendChild(newSelect);
-	document.getElementsByTagName("div")[1].appendChild(newForm);
+	newForm.appendChild(inputRange);
+	newForm.appendChild(dataList);
+
+	newSelect = document.createElement("div");
+	newSelect.setAttribute("id", "formSliderContent");
+	for(let index in arrTemp) {
+		valueOption = document.createElement("sup");
+		valueOption.innerHTML = "o";
+		newSelect.appendChild(valueOption);
+		valueOption = document.createTextNode(arrTemp[index]);
+		newSelect.appendChild(valueOption);
+	}
+	newForm.appendChild(newSelect);
+
+	document.getElementById("app").appendChild(newForm);
 }
 
 function WeatherForecast() {
-	console.log('weather forecast is showed');
 	let formsBlock;
-	let formCity;
-	let formTemperature;
 	let sliderBlock;
 
-	formsBlock = document.createElement("div");
-	formsBlock.setAttribute("id", "formsBlock");
-	document.getElementsByTagName("div")[0].appendChild(formsBlock);
+	formsBlock = new Form(cities, temperatures);
 
 	sliderBlock = document.createElement("div");
 	sliderBlock.setAttribute("id", "sliderBlock");
-	document.getElementsByTagName("div")[0].appendChild(sliderBlock);
-
-	formCity = new Form('city', cities);
-	formTemperature = new Form('temperature', temperatures);
+	document.getElementById("app").appendChild(sliderBlock);
 
 	sliderBlock.innerHTML = "Slider block"; // to do
 };
 
 function app() {
-	start = document.createElement("div");
+	start = document.createElement("section");
+	start.setAttribute("id", "app");
 	document.body.appendChild(start);
-	console.log('first div created');
 	wf = new WeatherForecast();
 };
 
-function loadData() {
-	alert("Data has loaded");
+function loadData(city) {
+	console.log("Data about "+city+" has loaded");
+	let xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("sliderBlock").innerHTML = "Data has been loading!";
+			showWeatherSlider(this);
+		} else {
+			document.getElementById("sliderBlock").innerHTML = "It has been loading!!!";
+		}
+	};
+	xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid="+apiKey+"&cnt=3", true);
+	xhttp.send();
+}
+
+function showWeatherSlider(xhttp) {
+	console.log(JSON.parse(xhttp.responseText));
+	document.getElementById("sliderBlock").innerHTML = xhttp.responseText;
 }
